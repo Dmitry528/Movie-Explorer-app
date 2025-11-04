8933756bc7b907736099bc7b7ddd196aa6e4f86c
Dmitry528 <dmytrochekurkov@gmail.com>
2025-11-04 16:23:20 +0200

    init router

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-const App = () => {
-  return <span className="text-3xl font-bold underline">Hello world</span>;
-};
-
-export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    tailwindcss(),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
